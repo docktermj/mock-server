41d40e663c31a2a6357ff77b10917bf03e88f795
Michael Dockter <michael@dockter.com>
2017-07-02 14:38:07 -0400

    Savepoint

--- subcommand/socket/main.go
+++ subcommand/socket/main.go
@@ -1,10 +1,32 @@
 package socket
 
 import (
-    "fmt"
-    
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/docopt/docopt-go"
 )
+
+func echoServer(c net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		nr, err := c.Read(buf)
+		if err != nil {
+			return
+		}
+
+		data := buf[0:nr]
+		println("Server got:", string(data))
+		_, err = c.Write(data)
+		if err != nil {
+			log.Fatal("Writing client error: ", err)
+		}
+	}
+}
 
 // Function for the "command pattern".
 func Command(argv []string) {
@@ -16,15 +38,44 @@ Usage:
 Options:
    -h, --help
    --socket-file=<file>        Socket file
+   --debug               Log debugging messages
 `
 
 	// DocOpt processing.
 
 	args, _ := docopt.Parse(usage, nil, true, "", false)
-	
-	fmt.Println(args)
+	socketFile := args["--socket-file"]
+	isDebug := args["--debug"]
 
+	// Listen on the Unix Domain Socket
 
+	if isDebug {
+		log.Printf("Starting echo server on ", socketFile)
+	}
 
+	ln, err := net.Listen("unix", socketFile)
+	if err != nil {
+		log.Fatal("Listen error: ", err)
+	}
+
+	//
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+	go func(ln net.Listener, c chan os.Signal) {
+		sig := <-c
+		log.Printf("Caught signal %s: shutting down.", sig)
+		ln.Close()
+		os.Exit(0)
+	}(ln, sigc)
+
+	for {
+		fd, err := ln.Accept()
+		if err != nil {
+			log.Fatal("Accept error: ", err)
+		}
+
+		go echoServer(fd)
+	}
 
 }
